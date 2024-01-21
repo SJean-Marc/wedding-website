218715e6b43b7864eedabd673045af077a9edb22
JM <6117901+SJean-Marc@users.noreply.github.com>
2024-01-21 01:47:13 +0100

    Ajout de la connexion vers Google sheet

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Arty et Magesh se marient ",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jun 7, 2024 19:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jun 8, 2024 03:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Château des hauts de provins',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "On a hâte de vous voir, si vous avez un souci merci de contacter X au XXXXXXXX."
         }
     });
 
@@ -212,26 +212,26 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Une seconde!</strong> En cours de sauvegarde.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        if (MD5($('#invite_code').val()) !== 'b59c67bf196a4758191e42f76670ceba'
+            && MD5($('#invite_code').val()) !== '934b535800b1cba8f96a5d72f72f1611') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Un souci!</strong> Le code est incorrecte.'));
         } else {
-            // $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
-            //     .done(function (data) {
-            //         console.log(data);
-            //         if (data.result === "error") {
-            //             $('#alert-wrapper').html(alert_markup('danger', data.message));
-            //         } else {
-            //             $('#alert-wrapper').html('');
-            //             $('#rsvp-modal').modal('show');
-            //         }
-            //     })
-            //     .fail(function (data) {
-            //         console.log(data);
-            //         $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-            //     });
+            $.post('https://script.google.com/macros/s/AKfycbxJwrLLcqQF7Oo62UIKA-cONnBVCjvvhMwaZ9bDAzQAGY8AmKFibPFnZ87br3YyVh-M/exec', data)
+                .done(function (data) {
+                    console.log(data);
+                    if (data.result === "error") {
+                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                    } else {
+                        $('#alert-wrapper').html('');
+                        $('#rsvp-modal').modal('show');
+                    }
+                })
+                .fail(function (data) {
+                    console.log(data);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Un souci!</strong> Une erreur de communication avec le serveur. '));
+                });
         }
     });
 
